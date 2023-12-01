e6a3a873863e698673758e43b9ef14f7c046f9c5
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2023-12-01 18:08:56 +0300

    Inputs

--- Assets/[GameFolders]/Scripts/Managers/InputManager.cs
+++ Assets/[GameFolders]/Scripts/Managers/InputManager.cs
@@ -1,8 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class InputManager : Singleton<InputManager>
 {
-
+    private InputActions input = null;
+    private Vector2 moveVector = Vector2.zero;
+    private void OnEnable()
+    {
+        LevelManager.Instance.OnLevelStart.AddListener(AddInputListeners);
+        LevelManager.Instance.OnLevelFinish.RemoveListener(RemoveInputListeners);
+    }
+    private void OnDisable()
+    {
+        LevelManager.Instance.OnLevelStart.RemoveListener(AddInputListeners);
+        LevelManager.Instance.OnLevelFinish.RemoveListener(RemoveInputListeners);
+    }
+    private void AddInputListeners()
+    {
+        input.Enable();
+        input.Player.WASD.performed += OnMovementPerformed;
+        input.Player.WASD.canceled += OnMovementCancelled;
+    }
+    private void RemoveInputListeners()
+    {
+        input.Disable();
+        input.Player.WASD.performed -= OnMovementPerformed;
+        input.Player.WASD.canceled -= OnMovementCancelled;
+    }
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        if (!LevelManager.Instance.IsLevelStarted)
+            return;
+        moveVector = value.ReadValue<Vector2>();
+    }
+    private void OnMovementCancelled(InputAction.CallbackContext value)
+    {
+        if (!LevelManager.Instance.IsLevelStarted)
+            return;
+        moveVector = Vector2.zero;
+    }
+    public Vector3 GetDirection()
+    {
+        Vector3 direction = new Vector3(moveVector.x, 0, moveVector.y);
+        return direction;
+    }
 }
